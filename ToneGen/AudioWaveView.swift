8e7f1b6949f981d7c63177a7ee861586675c904c
Rob Napier <robnapier@gmail.com>
2014-07-05 11:32:34 -0400

    Cleanups in drawing

--- ToneGen/AudioWaveView.swift
+++ ToneGen/AudioWaveView.swift
@@ -19,27 +19,29 @@ class AudioWaveView : UIView {
   init(coder aDecoder: NSCoder!) {
     super.init(coder:aDecoder)
     layer.borderWidth = 1
+    layer.geometryFlipped = true
   }
 
   override func drawRect(rect: CGRect) {
     if let wave = waveForm {
-      let firstChannel = wave.firstChannel()
+      let vals = wave.firstChannel()
+      let valCount = vals.count
 
-      let width = bounds.width
+      let width  = bounds.width
       let height = bounds.height
-      let zeroY = CGRectGetMidY(bounds)
-      let pixelsPerSample:Float = 1
+      let yZero  = Float(CGRectGetMidY(bounds))
+      let xScale = Float(1)
+      let yScale = Float(height/2)
 
       var path = UIBezierPath()
-      path.moveToPoint(CGPointMake(0, zeroY))
+      path.moveToPoint(CGPointMake(0, yZero))
 
       var x:CGFloat = 0
       while x < width {
-        for var t = 0; t < firstChannel.count; t++ {
-          path.addLineToPoint(CGPointMake(
-            x,
-            firstChannel[Int(t)] * Float(height/2) + Float(zeroY)))
-          x += pixelsPerSample
+        for var t = 0; x < width && t < valCount; t++ {
+          let y = vals[t] * yScale + yZero
+          path.addLineToPoint(CGPointMake(x, y))
+          x += xScale
         }
         path.stroke()
       }
